3be315f7314ff51f53b336bc54b43d6a5ea5ae0f
iSwiift <iSwiift@users.noreply.github.com>
2020-11-19 19:46:21 +1300

    Added a couple more garbage lines to test again.

--- GithubTest/HelloGit.cpp
+++ GithubTest/HelloGit.cpp
@@ -2,7 +2,9 @@
 
 int main() {
 
-	std::cout << "Hello Git Hub. Or is it Github. Testing test " << std::endl;
-	 
+	std::cout << "Hello Git Hub. Or is it Github. Testing test " << std::endl << std::endl;
+	std::cout << "I downloaded the Github desktop app and it looks like I need a text editor. Not sure why yet. Thought VS was it!";
+	std::cout << std::endl << std::endl;
+
 	return 0;
 }
